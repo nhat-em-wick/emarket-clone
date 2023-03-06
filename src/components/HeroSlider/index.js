6d47a4afbc20d649074745d1e41a3e05bb5924e7
Nguyen Minh Nhat <mnhat233@gmail.com>
2023-03-06 21:37:09 +0700

    test branch

--- src/components/HeroSlider/index.js
+++ src/components/HeroSlider/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, EffectFade } from 'swiper';
+import banner from '../../assets/images/tienlen.jpg'
 import 'swiper/css';
 import 'swiper/css/pagination';
 import "swiper/css/effect-fade";
@@ -30,7 +31,7 @@ const index = () => {
             className={cx('hero-slider__item')}
             style={{
               background:
-                'url(https://opencart.opencartworks.com/themes/so_emarket/layout4/image/cache/catalog/slideshow/home4/slide3-1680x500.webp) top center / cover no-repeat',
+                `url(${banner}) top center / cover no-repeat`,
             }}
           ></div>
         </SwiperSlide>
